cea3bbff904ca08b116ea4082f1a7d7837b79aa5
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2024-11-30 11:39:34 +0530

    breadcrum component added

--- frontend/src/Components/Items/Item.jsx
+++ frontend/src/Components/Items/Item.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import './Item.css'
+import React from "react";
+import "./Item.css";
+import { Link } from "react-router-dom";
 const Item = (props) => {
   return (
-    <div className='item'>
-      <img src={props.image} alt=''/>
+    <div className="item">
+      <Link to={`/product/${props.id}`}>
+        <img src={props.image} alt="" />
+      </Link>
+
       <p>{props.name}</p>
       <div className="item-prices">
-        <div className="item-price-new">
-        &#8377;{props.new_price} 
-        </div>
-        <div className="item-price-old">
-
-        &#8377;{props.old_price}          
-        </div>
-
+        <div className="item-price-new">&#8377;{props.new_price}</div>
+        <div className="item-price-old">&#8377;{props.old_price}</div>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
